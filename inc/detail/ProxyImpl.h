4180d24a3333ee8418a3a8bfc776d824cf683662
cnweaver <cnweaver@gmail.com>
2014-12-05 15:58:23 -0600

    Document more internals

--- inc/detail/ProxyImpl.h
+++ inc/detail/ProxyImpl.h
@@ -2,65 +2,95 @@
 #define SQUIDS_DETAIL_PROXYIMPL_H
 
 namespace detail{
+  ///Constant used to indicate that the first argument of a proxy was an
+  ///r-value reference and may be moved from
   constexpr static int Arg1Movable=1;
+  ///Constant used to indicate that the second argument of a proxy was an
+  ///r-value reference and may be moved from
   constexpr static int Arg2Movable=2;
   
+  ///Used to look up properties of operations on SU_vectors
   template<typename Op>
   struct operation_traits{
+    ///Indicates whether the calculation of each element of the result depends
+    ///only on the corresponding elements of the operands, meaning that the
+    ///calculation of the result elements may be reordered, or the inputs may
+    ///be overwritten
     constexpr static bool elementwise=false;
   };
   
+  ///The base class for objects representing arithmetic operations on SU_vectors.
+  ///Rather than eagerly performing arithemtic, the calculation's type and operands
+  ///are encoded in a proxy object which knows how to compute the operation once
+  ///more is known about the context (such as where the result will be stored).
+  ///This enables a number of operations which reduce copying of data.
   template<typename Op>
   struct EvaluationProxy{
-    const SU_vector& suv1;
-    const SU_vector& suv2;
+    const SU_vector& suv1; ///the first operand
+    const SU_vector& suv2; ///the second operand
     int flags;
     
+    ///perform the stored operation, storing the results in target
     template<typename VW>
     void compute(VW target) const{
       static_cast<const Op*>(this)->compute(target);
     }
+    ///compute the stored operation into a temporary SU_vector
     operator SU_vector() const{
       SU_vector result(suv1.dim);
       compute(detail::vector_wrapper<detail::AssignWrapper>{result.dim,result.components});
       return(result);
     }
     
+    ///multiplication by scalars
     SU_vector operator*(double a) const;
+    ///addition with SU_vectors
     SU_vector operator+(const SU_vector& other) const;
+    ///subtraction with SU_vectors
     SU_vector operator-(const SU_vector& other) const;
-    SU_vector SUEvolve(const SU_vector& other ,double t) const;
+    ///time evolution according to an SU_vector operator
+    SU_vector SUEvolve(const SU_vector& other, double t) const;
     
+    ///scalar product between proxies
     template<typename ProxyType, REQUIRE_EVALUATION_PROXY>
     double operator*(const ProxyType& other) const{
       return(SU_vector(*this)*SU_vector(other));
     }
+    ///addition of proxies
     template<typename ProxyType, REQUIRE_EVALUATION_PROXY>
     SU_vector operator+(const ProxyType& other) const;
+    ///subtraction of proxies
     template<typename ProxyType, REQUIRE_EVALUATION_PROXY>
     SU_vector operator-(const ProxyType& other) const;
+    ///time evolution of a proxy according to an operator given by another proxy
     template<typename ProxyType, REQUIRE_EVALUATION_PROXY>
     SU_vector SUEvolve(const ProxyType& other ,double t) const;
     
+    ///whether the result of the operation can be written directly
+    ///into the storage of the first operand
     bool mayStealArg1() const{
       return(flags&detail::Arg1Movable && operation_traits<Op>::elementwise);
     }
   };
   
+  ///The result of a time evolution operation
   struct EvolutionProxy : public EvaluationProxy<EvolutionProxy>{
-    double t;
+    double t; ///the time over which the evolution is performed
     
+    ///evolve suv1 according to the operator suv2 over time period t
     EvolutionProxy(const SU_vector& suv1,const SU_vector& suv2,double t):
     EvaluationProxy<EvolutionProxy>{suv1,suv2,0},t(t){}
     
     template<typename VW>
     void compute(VW target) const{
-      auto& suv_new=target;
+      auto& suv_new=target; //alias for the name expected by generated code
 #include "../SU_inc/EvolutionSelect.txt"
     }
   };
   
+  ///The result of adding two SU_vectors
   struct AdditionProxy : public EvaluationProxy<AdditionProxy>{
+    ///THe sum of suv1 and suv2
     AdditionProxy(const SU_vector& suv1,const SU_vector& suv2,int flags=0):
     EvaluationProxy<AdditionProxy>{suv1,suv2,flags}{}
     
@@ -76,7 +106,9 @@ namespace detail{
     constexpr static bool elementwise=true;
   };
   
+  ///The result of subtracting two SU_vectors
   struct SubtractionProxy : public EvaluationProxy<SubtractionProxy>{
+    ///The difference of suv1 and suv2
     SubtractionProxy(const SU_vector& suv1,const SU_vector& suv2,int flags=0):
     EvaluationProxy<SubtractionProxy>{suv1,suv2,flags}{}
     
@@ -92,9 +124,11 @@ namespace detail{
     constexpr static bool elementwise=true;
   };
   
+  ///The result of multiplying an SU_vector by a scalar
   struct MultiplicationProxy : public EvaluationProxy<MultiplicationProxy>{
-    double a;
+    double a; ///scalar mutiplcation factor
     
+    ///The product of suv1 and a
     MultiplicationProxy(const SU_vector& suv1,double a,int flags=0):
     EvaluationProxy<MultiplicationProxy>{suv1,suv1,flags},a(a){}
     
@@ -110,7 +144,9 @@ namespace detail{
     constexpr static bool elementwise=true;
   };
   
+  ///The result of i times the commutator of two SU_vectors
   struct iCommutatorProxy : public EvaluationProxy<iCommutatorProxy>{
+    ///The commutator of suv1 and suv2
     iCommutatorProxy(const SU_vector& suv1,const SU_vector& suv2):
     EvaluationProxy<iCommutatorProxy>{suv1,suv2,0}{}
     
@@ -121,7 +157,9 @@ namespace detail{
     }
   };
   
+  ///The result of the anticommutator of two SU_vectors
   struct ACommutatorProxy : public EvaluationProxy<ACommutatorProxy>{
+    ///The anticommutator of suv1 and suv2
     ACommutatorProxy(const SU_vector& suv1,const SU_vector& suv2):
     EvaluationProxy<ACommutatorProxy>{suv1,suv2,0}{}
     
@@ -130,8 +168,6 @@ namespace detail{
 #include "../SU_inc/AnticommutatorSelect.txt"
     }
   };
-  
-  
   
   template<typename Op>
   SU_vector EvaluationProxy<Op>::operator*(double a) const{
